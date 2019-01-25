fc74e2f771e0db5e0affd7ec31276c793d3fe8c1
Simon Rowe <srowe@pivotal.io>
2019-01-25 13:16:03 +0000

    Fix Security of actuator in portfolio service.

--- src/main/java/io/pivotal/portfolio/config/SecurityConfig.java
+++ src/main/java/io/pivotal/portfolio/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.pivotal.portfolio.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -22,8 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .anonymous().disable()
-                .authorizeRequests().anyRequest().fullyAuthenticated()
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                .anyRequest().fullyAuthenticated()
                 .and().oauth2Client()
                 .and().oauth2ResourceServer()
                 .jwt().jwtAuthenticationConverter(grantedAuthoritiesConverter).decoder(jwtDecoder());
